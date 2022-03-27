574124678725db57afd3dbd457992128d6c64d7b
Maxime Spagnolo <maxime.spagnolo@interactiv-group.com>
2022-03-27 01:13:16 +0100

    Update movies.js

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,23 +1,30 @@
-const moviesJson = require('../movies.json')
 const path = require('path')
 const fs = require('fs')
 
-const movies = typeof moviesJson === 'string' ? JSON.parse(moviesJson) : [...moviesJson]
+const filePath = path.join(process.cwd(), '/server/movies.json')
+
+const readFile = () => {
+  const moviesJson = fs.readFileSync(filePath, 'utf-8')
+  return typeof moviesJson === 'string' ? JSON.parse(moviesJson) : [...moviesJson]
+}
 
 const getMovies = () => (req, res) => {
+  const movies = readFile()
   res.json(movies)
 }
 
 const addMovies = () => (req, res) => {
+  const movies = readFile()
   const id = movies[movies.length - 1].id + 1
   const newMovie = { ...req.body, id }
   const newMovies = [...movies, newMovie]
-  fs.writeFileSync(path.join(process.cwd(), '/server/movies.json'), JSON.stringify(newMovies))
+  fs.writeFileSync(filePath, JSON.stringify(newMovies))
   const result = { collection: newMovies, id }
   res.json(result)
 }
 
 const updateMovies = () => (req, res) => {
+  const movies = readFile()
   const idx = movies.findIndex(m => m.id === +req.params.id)
   const newMovies = [...movies.slice(0, idx), req.body, ...movies.slice(idx + 1)]
   fs.writeFileSync(path.join(process.cwd(), '/server/movies.json'), JSON.stringify(newMovies))
@@ -25,6 +32,7 @@ const updateMovies = () => (req, res) => {
 }
 
 const removeMovies = () => (req, res) => {
+  const movies = readFile()
   const idx = movies.findIndex(m => m.id === +req.params.id)
   const newMovies = [...movies.slice(0, idx), ...movies.slice(idx + 1)]
   fs.writeFileSync(path.join(process.cwd(), '/server/movies.json'), JSON.stringify(newMovies))
